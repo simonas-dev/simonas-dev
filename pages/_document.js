028b27dfc98cf46528df7164468007de1e2467e0
Simonas Sankauskas <simonas.sank@gmail.com>
2023-08-03 21:47:16 +0300

    Some content

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Simonas Sankauskas Blog',
+    description: 'Something might happen here in the future.'
   }
 
   return (
@@ -15,12 +14,9 @@ export default function Document() {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@SimonasSank" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
         <Main />
